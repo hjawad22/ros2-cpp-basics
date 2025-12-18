1ce4361abdf6c8b074f7a5595597d383b13a9292
hjawad22 <houdajawad@gmail.com>
2025-12-18 02:53:03 +0000

    Fix GitHub Pull Request template

--- ros2_ws/src/listener.cpp
+++ ros2_ws/src/listener.cpp
@@ -17,8 +17,12 @@ public:
 private:
   void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
   {
-    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
-  }
+  RCLCPP_INFO_STREAM(
+  this->get_logger(),
+  "I heard: '" << msg->data << "'"
+);
+
+}
 
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
