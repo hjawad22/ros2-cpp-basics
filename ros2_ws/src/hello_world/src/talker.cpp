ce8ecedf2f1690ee9cf4538b7282c2dd1383b871
hjawad22 <houdajawad@gmail.com>
2025-12-21 03:03:37 +0000

    Add talker node code to transmit to listener node

--- ros2_ws/src/hello_world/src/talker.cpp
+++ ros2_ws/src/hello_world/src/talker.cpp
@@ -1,15 +1,39 @@
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+class Talker : public rclcpp::Node
+
+{
+public:
+  Talker() : Node("talker")
+  {
+    publisher_ = this->create_publisher<std_msgs::msg::String>("chatter", 10);
+
+    timer_ = this->create_wall_timer(
+      1s,
+      std::bind(&Talker::publish_message, this));
+  }
+
+private:
+  void publish_message()
+  {
+    auto message = std_msgs::msg::String();
+    message.data = "Hello from my first ROS 2 talker!";
+    // Using printf-style formatting
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+  }
+
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-
-  auto node = rclcpp::Node::make_shared("hello_node");
-
-  RCLCPP_INFO(node->get_logger(), "Hello from my very first ROS 2 C++ node!");
-
-  rclcpp::spin(node);
+  rclcpp::spin(std::make_shared<Talker>());
   rclcpp::shutdown();
-
   return 0;
 }
